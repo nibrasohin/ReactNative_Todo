23c5cfdedab234db6eed88935e1b281925d0b03e
ohin94 <nibras.ohin@yahoo.com>
2018-05-23 23:13:47 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './app/components/Main'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
